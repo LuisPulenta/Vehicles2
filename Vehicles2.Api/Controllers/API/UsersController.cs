34287014919f0ae04d3f38dd32196959c9b6e594
LuisPulenta <luisalbertonu@gmail.com>
2021-11-04 10:02:06 -0300

    UsersController

---
+++ Vehicles2.Api/Controllers/API/UsersController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles2.Api.Data;
+using Vehicles2.Api.Data.Entities;
+using Vehicles2.Common.Enums;
+
+namespace Vehicles2.Api.Controllers.API
+{
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users
+                .Include(x => x.DocumentType)
+                .OrderBy(x => x.LastName)
+                .ThenBy(x => x.FirstName)
+                .Where(x => x.UserType == UserType.User)
+                .ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(string id)
+        {
+            User user = await _context.Users
+                .Include(x => x.DocumentType)
+                .Include(x => x.Vehicles)
+                .ThenInclude(x => x.VehicleType)
+                .Include(x => x.Vehicles)
+                .ThenInclude(x => x.Brand)
+                .Include(x => x.Vehicles)
+                .ThenInclude(x => x.VehiclePhotos)
+                .Include(x => x.Vehicles)
+                .ThenInclude(x => x.Histories)
+                .ThenInclude(x => x.Details)
+                .ThenInclude(x => x.Procedure)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return user;
+        }
+    }
+}
